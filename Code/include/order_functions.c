06fd2320d04cc2bfee2e43569f7ca5616f622a08
CristianS2004 <83604406+CristianS2004@users.noreply.github.com>
2023-11-23 11:45:16 -0500

    Update order_functions.c
    Fixed order function

--- Code/include/order_functions.c
+++ Code/include/order_functions.c
@@ -3,32 +3,38 @@ This file contains all functions and constants used to take orders
 */
 
 // takes the customers order
-void takeOrder() 
+void takeOrder()
 {
-    string order_kind = " ";
 
-    do {
+    string order_kind = " ";
 
     displayString(3, "Please scan your order");
 
-    if(SensorValue[S3] = (int)colorRed) {
+    while(order_kind == " "){
+    if(SensorValue[S1] == (int)colorRed) {
     order_kind = "Coke";
-
-    else if(SensorValue[S3] = (int)colorBlue)
+    }
+    else if(SensorValue[S1] == (int)colorBlue)
+    {
     order_kind = "Raspberry Lemonade";
-
-    else if(SensorValue[S3] = (int)colorYellow)  
+    }
+    else if(SensorValue[S1] == (int)colorYellow)
+    {
     order_kind = "Lemonade";
+    }
+    wait1Msec(2000);
 
-    else displayString(3, "Not a valid order type");
-
-    } while(order_kind = " ");
-
-    } // takeOrder
-
-    displayString(3, "Please make %f" order_kind);
-    
-}
+  }
+//This if statement should go outside the function to ensure the string will stay while drivin back
+  if(order_kind == "Coke" || order_kind == "Raspberry Lemonade" || order_kind == "Lemonade")
+    {
+        displayString(3, order_kind);
+        wait1Msec(10000);
+    }else
+    {
+        displayString(3, "No valid order");
+        wait1Msec(10000);
+    }
 
 // makes a bell noise and delivers the order to the table
 void orderUp(int tableNumber) {
