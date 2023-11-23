98ebc3d10f41d5ff61c192e600b3c8370717bc46
Szabo <alexanderszabodefi@gmail.com>
2023-11-23 16:09:54 -0500

    updating order functions

--- Code/include/order_functions.c
+++ Code/include/order_functions.c
@@ -3,14 +3,15 @@ This file contains all functions and constants used to take orders
 */
 
 // takes the customers order
-void takeOrder()
+string takeOrder()
 {
 
-    string order_kind = " ";
+    string order_kind;
 
     displayString(3, "Please scan your order");
 
-    while(order_kind == " "){
+    do 
+    {
     if(SensorValue[S1] == (int)colorRed) {
     order_kind = "Coke";
     }
@@ -22,23 +23,25 @@ void takeOrder()
     {
     order_kind = "Lemonade";
     }
-    wait1Msec(2000);
+    else
+    displayString(3, "Sorry we do not have that drink");
+    } while (order_kind != "Coke" || order_kind != "Raspberry Lemonade" || order_kind != "Lemonade");
+      
+    return order_kind;
+    
+}
 
-  }
-//This if statement should go outside the function to ensure the string will stay while drivin back
-  if(order_kind == "Coke" || order_kind == "Raspberry Lemonade" || order_kind == "Lemonade")
-    {
-        displayString(3, order_kind);
-        wait1Msec(10000);
-    }else
-    {
-        displayString(3, "No valid order");
-        wait1Msec(10000);
-    }
 
 // makes a bell noise and delivers the order to the table
-void orderUp(int tableNumber) {
-        while(!SensorValue[S1]) {}
+void orderUp(int tableNumber, string order_kind) {
+        // wait for enter button to be released (p2)
+	while(!getButtonPress(buttonEnter)) 
+  {
+    displayString(3, "%s", order_kind);
+	}
+
+	while(getButtonPress(buttonEnter)) {
+	}
         wait1MSec(200);
         goToTable(tableNumber);
     }
